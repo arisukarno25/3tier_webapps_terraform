fb3abb903ac46d6b39a36f38f3d3a99c633db683
arisukarno25 <arisukarno.25@gmail.com>
2025-04-05 21:27:09 +0700

    add directory structure for dev and prod

---
+++ backends/dev/dev_backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    # remote backend
+    bucket = "terraform-3tierwebapps-state"
+    key = "env/dev/terraform.tfstate"
+    region = "ap-southeast-2"
+
+    # state lock 
+    dynamodb_table = "terraform-3tierwebapps-dev-state"
+  }
+}
---
+++ backends/prod/prod_backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    # remote backend
+    bucket = "terraform-3tierwebapps-state"
+    key = "env/prod/terraform.tfstate"
+    region = "ap-southeast-2"
+
+    # state lock 
+    dynamodb_table = "terraform-3tierwebapps-prod-state"
+  }
+}
