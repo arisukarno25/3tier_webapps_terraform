6c2a62c9d80fafe506dc5a6c5c01fa64c1eaa128
arisukarno25 <arisukarno.25@gmail.com>
2025-04-05 22:01:28 +0700

    update version.tf

--- versions.tf
+++ versions.tf
@@ -2,13 +2,13 @@ terraform {
   required_version = "~> 1.6"
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.90.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.90.0"
     }
   }
 }
 
 provider "aws" {
   profile = "default"
-  region = "ap-southeast-2"
+  region  = "ap-southeast-2"
 }
