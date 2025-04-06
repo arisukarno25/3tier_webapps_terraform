9b45834b5d2f88dcf289156e0da5793c643427d3
arisukarno25 <arisukarno.25@gmail.com>
2025-04-06 17:46:50 +0700

    update main.tf due to change in variable

--- modules/rds-mysql/main.tf
+++ modules/rds-mysql/main.tf
@@ -2,9 +2,9 @@ resource "aws_rds_cluster" "rds-mysql" {
   cluster_identifier        = var.db-identier
   engine                    = var.engine
   engine_version            = var.engine_version
-  database_name             = var.database
-  master_username           = var.dbusername
-  master_password           = var.dbpassword
+  database_name             = var.database_name
+  master_username           = var.db_username
+  master_password           = var.db_password
   db_cluster_instance_class = var.db_cluster_instance_class
   allocated_storage         = var.db_storage
   #   vpc_security_group_ids = var.db_vpc_security_group
