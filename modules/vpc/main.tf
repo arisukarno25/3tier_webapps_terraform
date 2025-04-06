2a473e7e62f423089cf13f7c39e28f7cb57a6ae3
arisukarno25 <arisukarno.25@gmail.com>
2025-04-06 17:32:46 +0700

    update main.tf in vpc module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr 
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 
   tags = {
     Name = "${var.name}-vpc"
@@ -17,9 +17,9 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_subnet" "public" {
-  count = length(var.public_subnet)
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.public_subnet[count.index]
+  count             = length(var.public_subnet)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet[count.index]
   availability_zone = var.availability_zone[count.index]
 
   tags = {
@@ -28,9 +28,9 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-  count = length(var.private_subnet)
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.private_subnet[count.index]
+  count             = length(var.private_subnet)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet[count.index]
   availability_zone = var.availability_zone[count.index]
 
   tags = {
@@ -40,8 +40,8 @@ resource "aws_subnet" "private" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id = aws_subnet.public[0].id
-  depends_on = [ aws_internet_gateway.igw ]
+  subnet_id     = aws_subnet.public[0].id
+  depends_on    = [aws_internet_gateway.igw]
 
   tags = {
     Name = "${var.name}-nat"
