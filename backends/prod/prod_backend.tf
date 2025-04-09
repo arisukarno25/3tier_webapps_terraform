91397509e6376ce864a0ab0f404b0aacc425ecda
arisukarno25 <arisukarno.25@gmail.com>
2025-04-09 14:27:18 +0700

    update key backend for prod

--- backends/prod/prod_backend.tf
+++ backends/prod/prod_backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     # remote backend
     bucket = "terraform-3tierwebapps-state"
-    key = "env/prod/terraform.tfstate"
+    key    = "env/prod/terraform.tfstate"
     region = "ap-southeast-2"
 
     # state lock 
